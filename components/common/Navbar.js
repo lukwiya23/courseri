38d07e6667339aa818ae5fe12062550984b95bfe
lukwiya23 <luk23bonnie8@gmail.com>
2022-03-23 13:24:19 +0300

    .

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -7,17 +7,16 @@ const Navbar = () => {
                 <nav className="relative" aria-label="Global">
                     <div className="flex justify-between">
                         <div>
-                            <Link href="/">  
-                            <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Home</a>
+                            <Link href="/">
+                                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Home</a>
                             </Link>
-                            <Link href="/">  
-                            <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Courses</a>
+                            <Link href="/">
+                                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Courses</a>
                             </Link>
 
                         </div>
                         <div>
-                            <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Register</a>
-                            <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Login</a>
+                            <a className="font-medium px-8 rounded-md py-3 bg-indigo-600 mr-8 text-white cursor-pointer hover:text-gray-300 text-base border">Connect Wallet</a>
                         </div>
                     </div>
                 </nav>
--- components/course/curriculum/index.js
+++ components/course/curriculum/index.js
@@ -50,7 +50,7 @@ const Curriculum = ({locked}) => {
                                                 </span>
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Play</a>
+                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">{locked ? "Get Access" : "Play"}</a>
                                             </td>
                                         </tr>
                                     )}
